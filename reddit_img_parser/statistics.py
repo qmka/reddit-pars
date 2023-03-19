79f0bc64755cc877e35d3d8a43f0647e1a0f70bb
qmka <eidolonzx@gmail.com>
2023-03-19 17:53:15 +0400

    feat: add statistics to subreddits

--- reddit_img_parser/statistics.py
+++ reddit_img_parser/statistics.py
@@ -1,61 +1,41 @@
-import praw
-import os
-
-from dotenv import load_dotenv
-from progress.spinner import Spinner
+from reddit_img_parser.reddit import get_reddit_entry, get_submissions
 
 
-class DotSpinner(Spinner):
-    phases = [' ', '...']
-
-
-def make_reddit_instance():
-    load_dotenv()
-    CLIENT_ID = os.getenv('CLIENT_ID')
-    CLIENT_SECRET = os.getenv('CLIENT_SECRET')
-    USER_AGENT = os.getenv('USER_AGENT')
-
-    return praw.Reddit(
-        client_id=CLIENT_ID,
-        client_secret=CLIENT_SECRET,
-        user_agent=USER_AGENT)
-
-def get_posters_list(subreddit_name, category, time_filter, limit):
+def get_statistics(type, name, category, time_filter, limit):
     
-    print('Connecting to Reddit... ', end='')
-    reddit = make_reddit_instance()
-    entry = reddit.subreddit(subreddit_name)
-    print('Success!')
+    entry = get_reddit_entry(type, name)
+    if not entry:
+        return
 
-    if category == 'hot':
-        submissions = entry.hot(limit=limit)
-    elif category == 'new':
-        submissions = entry.new(limit=limit)
-    elif category == 'rising':
-        submissions = entry.rising(limit=limit)
-    else:
-        submissions = entry.top(limit=limit, time_filter=time_filter)
+    submissions = get_submissions(entry, type, category, limit, time_filter)
+    if not submissions:
+        return
     
-    posters = {}
+    statistics_data = {}
 
-    print('Processing posts... ')
+    print('Processing... ')
     
     for sub in submissions:
-
-        sub_author = sub.author
-        if not sub_author:
-            continue
-
-        author_name = sub_author.name
-        if not author_name in posters:
-            posters[author_name] = 1
+        if type == 'redditor':
+            key_entry = sub.subreddit
         else:
-            posters[author_name] += 1
+            key_entry = sub.author
 
-    print(f"Result: redditors that posted to {subreddit_name} more than 2 times")
-    print((51 + len(subreddit_name)) * '-')
-    sorted_posters = dict(sorted(posters.items(), key=lambda item: item[1]))
-    for user in sorted_posters: 
-        count = sorted_posters[user]
-        if count > 2:
-            print(f"{user}: {count}")
+        if not key_entry:
+            continue
+        
+        if type == 'redditor':
+            key_name = key_entry.display_name
+        else:
+            key_name = key_entry.name
+
+        if not key_name in statistics_data:
+            statistics_data[key_name] = 1
+        else:
+            statistics_data[key_name] += 1
+
+    print(f"Statistics to: {name}")
+    print((21 + len(name)) * '-')
+    sorted_data = dict(sorted(statistics_data.items(), key=lambda item: -item[1]))
+    for el in sorted_data: 
+        print(f"{el}: {sorted_data[el]}")
