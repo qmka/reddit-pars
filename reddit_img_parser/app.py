4a03633623c8b4a4955c1f99cf9c0720087586c8
qmka <eidolonzx@gmail.com>
2023-03-19 17:53:37 +0400

    refactor: get out reddit funcs

--- reddit_img_parser/app.py
+++ reddit_img_parser/app.py
@@ -1,68 +1,12 @@
-import praw
-import prawcore.exceptions
-import os
-
-from dotenv import load_dotenv
-
 from reddit_img_parser.utils import log, make_folder, convert_unix_time
 from reddit_img_parser.download import get_file
-
-
-'''
-def is_valid_reddit_instance(instance, type):
-    if type == 'subreddit':
-        try:
-            instance.id
-            return True
-        except prawcore.exceptions.Redirect:
-            return False
-        except prawcore.exceptions.NotFound:
-            return False
-    elif type == 'redditor':
-        try:
-            instance.name
-            return True
-        except prawcore.exceptions.NotFound:
-            return False
-        except prawcore.exceptions.Forbidden:
-            return False
-    else:
-        raise ValueError("Invalid object type. Must be either 'redditor' or 'subreddit'.")
-'''
-def is_valid_reddit_instance(instance, obj_type):
-    try:
-        if obj_type == 'subreddit':
-            instance.id
-        elif obj_type == 'redditor':
-            instance.name
-        else:
-            raise ValueError("Invalid object type. Must be either 'redditor' or 'subreddit'.")
-        return True
-    except (prawcore.exceptions.Redirect, prawcore.exceptions.NotFound, prawcore.exceptions.Forbidden):
-        return False
-
-
-def make_reddit_instance():
-    load_dotenv()
-    CLIENT_ID = os.getenv('CLIENT_ID')
-    CLIENT_SECRET = os.getenv('CLIENT_SECRET')
-    USER_AGENT = os.getenv('USER_AGENT')
-
-    return praw.Reddit(
-        client_id=CLIENT_ID,
-        client_secret=CLIENT_SECRET,
-        user_agent=USER_AGENT)
+from reddit_img_parser.reddit import get_reddit_entry, get_submissions
 
 
 def parse(type, name, category, time_filter, limit):
-    reddit = make_reddit_instance()
-
-    if type == 'redditor':
-        entry = reddit.redditor(name)
-    elif type == 'subreddit':
-        entry = reddit.subreddit(name)
-    if not is_valid_reddit_instance(entry, type):
-        print(f"Invalid {type} name, try again!")
+    
+    entry = get_reddit_entry(type, name)
+    if not entry:
         return
 
     path = make_folder(name, category, time_filter)
@@ -75,19 +19,9 @@ def parse(type, name, category, time_filter, limit):
         log("Incorrect Reddit entry {name} (doesn't exists, suspended or something else).", name=name)
         return
 
-    # Make submissions list
-    if category == 'hot':
-        submissions = entry.hot(limit=limit)
-    elif category == 'new':
-        submissions = entry.new(limit=limit)
-    elif category == 'rising':
-        if type == 'redditor':
-            log("'Rising' category doesn't exists for redditors, try different!")
-            return
-        else:
-            submissions = entry.rising(limit=limit)
-    else:
-        submissions = entry.top(limit=limit, time_filter=time_filter)
+    submissions = get_submissions(entry, type, category, limit, time_filter)
+    if not submissions:
+        return
 
     # Show info
     log("Name of {type}: {name}", type=type, name=name)
