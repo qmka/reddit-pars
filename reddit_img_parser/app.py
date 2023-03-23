caa53ccec9d8680bcb29c6e17c9e6abe36aa89df
qmka <eidolonzx@gmail.com>
2023-03-23 16:35:19 +0400

    refactor: parse func

--- reddit_img_parser/app.py
+++ reddit_img_parser/app.py
@@ -3,6 +3,31 @@ from reddit_img_parser.download import get_file
 from reddit_img_parser.reddit import get_reddit_entry, get_submissions
 
 
+def parse_submissions(submissions, type, path):
+    for i, sub in enumerate(submissions):
+        if type == 'subreddit':
+            title, author, url = sub.title, sub.author, sub.url
+        elif type == 'redditor':
+            sub_attrs = vars(sub)
+            posted_to = sub.subreddit
+            title = sub_attrs.get('link_title') or sub_attrs.get('title')
+            url = sub_attrs.get('link_url') or sub_attrs.get('url')
+
+        sub_created = convert_unix_time(sub.created)
+        print('------------------------------------')
+        log('#{counter}', counter=i+1)
+        log('TITLE: {title}', title=title)
+        log('CREATED AT: {sub_created}', sub_created=sub_created)
+        log('URL: {url}', url=url)
+
+        if type == 'subreddit':
+            log('AUTHOR: {author}', author=author)
+        elif type == 'redditor':
+            log('POSTED TO: {posted_to}', posted_to=posted_to)
+
+        get_file(url, path)
+
+
 def parse(**params):
     type = params.get('parse_type', None)
     name = params.get('name', None)
@@ -34,36 +59,11 @@ def parse(**params):
     if not submissions:
         return
 
-    # Show info
     log("Name of {type}: {name}", type=type, name=name)
     log("Account created: {created}", created=created)
     log("Processing..." )
 
-    # Parse each submission
-    for i, sub in enumerate(submissions):
-
-        if type == 'subreddit':
-            title, author, url = sub.title, sub.author, sub.url
-        elif type == 'redditor':
-            sub_attrs = vars(sub)
-            posted_to = sub.subreddit
-            title = sub_attrs.get('link_title') or sub_attrs.get('title')
-            url = sub_attrs.get('link_url') or sub_attrs.get('url')
-
-        sub_created = convert_unix_time(sub.created)
-        print('------------------------------------')
-        log('#{counter}', counter=i+1)
-        log('TITLE: {title}', title=title)
-        log('CREATED AT: {sub_created}', sub_created=sub_created)
-        log('URL: {url}', url=url)
-
-        if type == 'subreddit':
-            log('AUTHOR: {author}', author=author)
-        elif type == 'redditor':
-            log('POSTED TO: {posted_to}', posted_to=posted_to)
-
-        get_file(url, path)
-
+    parse_submissions(submissions, type, path)
     log('Parsing completed!')
 
 
@@ -75,8 +75,7 @@ def batch_parse(**params):
     limit = params.get('limit', None)
 
     print('Preparing to batch parse...')
-    # 1. Читаем из файла в список
-    # 2. Для каждого из списка вызываем parse
+
     try:
         with open(filename, "r") as f:
             entries = f.read().splitlines()
@@ -84,7 +83,6 @@ def batch_parse(**params):
         print("Batch file doesn't exists!")
         return
 
-    # Print the list of usernames
     for entry_name in entries:
         entry_params = {
             'name': entry_name,
